23d6428d4b61c325a1554d1924ec1e0b4c4d2997
Bathula Ganesh <159684527+Ganesh-a0576@users.noreply.github.com>
2025-09-18 09:37:07 +0530

    Update Image.js

--- src/Assets/Images/Image.js
+++ src/Assets/Images/Image.js
@@ -2,10 +2,11 @@ export let payment = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838
 export let profile = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838912/vecteezy_user-avatar-line-style__x94mjc.jpg';
 export let MaleCloth = 'https://i.pinimg.com/736x/e5/6b/79/e56b799b365e63c41041feb38fb7e965--guy-models-men-fashion.jpg';
 export let FemaleCloth = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838912/Cloths_kpwmp2.jpg';
-export let GroupCloth = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Cloth-banner_ylavk9.jpg';
+export let GroupCloth = 'https://images.pexels.com/photos/11597709/pexels-photo-11597709.jpeg';
 export let Electronics = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Electronics_lrjvlg.jpg';
 export let Jewelry = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/model-jwellery_qlcjog.jpg';
 export let Shoes = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Shoe_e2yc1d.jpg';
 export let Books = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838761/Book_lc6ikb.jpg';
 export let EmptyCart = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838909/Cart_bk4xgl.jpg';
+
 export let customerReview = "https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Review_bm6chw.jpg";
